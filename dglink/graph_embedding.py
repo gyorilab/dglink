45c6f561d34dc8c45d7d412d49b7b00a6c938e1f
buz galbraith <buzgalbraith@gmail.com>
2025-09-25 15:09:23 -0400

    adding current state of graph embedig code

--- dglink/graph_embedding.py
+++ dglink/graph_embedding.py
@@ -1,16 +1,16 @@
 """
 goal is to train an embedding model and use that as a proxy for related study prediction.
-
-going to w
 """
 
 from pykeen.triples import TriplesFactory
+from pykeen.triples.utils import load_triples
 from pykeen.pipeline import pipeline
 import torch
 import numpy as np
 from itertools import combinations
 import gzip
 import torch
+import pandas as pd
 
 
 all_project_ids = [
@@ -18,19 +18,19 @@ all_project_ids = [
     "syn5562324",  ## small project
     "syn27761862",  ## small project
     "syn4939874",  ## large project
-    # "syn4939876",  ## locked
+    "syn4939876",  ## locked
     "syn4939906",  ## small
-    # "syn4939916",  ## locked
+    "syn4939916",  ## locked
     "syn7217928",  ## large
     "syn8016635",  ## small
-    # "syn11638893",  ## locked
+    "syn11638893",  ## locked
     "syn11817821",  ## large
-    # "syn21641813",  ## locked
-    # "syn21642027",  ## locked
+    "syn21641813",  ## locked
+    "syn21642027",  ## locked
     "syn21650493",  ## large
     "syn21984813",  ## large
-    # "syn23639889",  ## locked
-    # "syn51133914",  ## locked
+    "syn23639889",  ## locked
+    "syn51133914",  ## locked
     "syn52740594",  ## large
 ]
 
@@ -38,23 +38,22 @@ all_project_ids = [
 def train_embedding_model(
     edge_path="dglink/resources/edges.tsv",
     model_name="TransE",
-    epochs=20,
+    epochs=100,
     save=True,
     save_path="dglink/resources/embedding_test",
 ):
     """Trains a network embedding model with the PyKEEN pipeline. Return the model and entity_to_id mapping"""
     ## split the dataset
-    tf = TriplesFactory.from_path(edge_path)
+    # tf = TriplesFactory.from_path(edge_path)
+    triples = load_triples(path = edge_path, column_remapping = (0, 2, 1))     
+    tf = TriplesFactory.from_labeled_triples(triples[1:]) ## drop header 
     training, testing = tf.split()
-    training, testing, validation = tf.split([0.8, 0.1, 0.1])
-
+    # training, testing, validation = tf.split([0.8, 0.1, 0.1])
     ## train the model
     result = pipeline(
         training=training,
         testing=testing,
-        validation=validation,
         model=model_name,
-        stopper="early",
         epochs=epochs,
     )
     if save:
@@ -90,25 +89,82 @@ def id_to_embedding(entity_to_id, model, id_1, id_2):
 
 def l2(a, b):
     """simple l2 distance metric"""
-    res = np.sum((a - b) ** 2)
-    return np.sqrt(res)
 
+    return np.linalg.norm(a-b)
+
+def cosine_dist(a,b):
+    sim = np.sum(a*b)/(np.linalg.norm(a) * np.linalg.norm(b))
+    return 1 - sim
+def check_related_study_exists(edges_df, id_1, id_2):
+    """checks if a pair of studies has a related edge"""
+    df = edges_df[edges_df[':TYPE'] == 'has_relatedStudies']
+    forward_df = df[(df[':START_ID'] == id_1) & (df[':END_ID'] == id_2)]
+    backward_df = df[(df[':START_ID'] == id_2) & (df[':END_ID'] == id_1)]
+    return (len(forward_df) + len(backward_df)) > 0
 
 if __name__ == "__main__":
-    train = False
+    train = True
+    model_name = 'RotatE' #'TransE'
     if train:
         model, entity_to_id = train_embedding_model(
-            edge_path="dglink/resources/pulled_edges_whole.tsv",
-            model_name="TransE",
-            epochs=20,
+            edge_path="dglink/resources/edges_with_no_related.tsv",
+            model_name=model_name,
+            epochs=250,
         )
     else:
         model, entity_to_id = load_embedding_model(
             save_path="dglink/resources/embedding_test"
         )
+    res = []
+    ## TODO: remove later 
+    edges_df = pd.read_csv("dglink/resources/edges_with_related.tsv", sep='\t')
+    ## TODO end of remove
+    x = 0 
     for id_1, id_2 in combinations(all_project_ids, 2):
         embed_1, embed_2 = id_to_embedding(
             entity_to_id=entity_to_id, model=model, id_1=id_1, id_2=id_2
         )
         distance = l2(embed_1, embed_2)
-        print(f"distance between projects {id_1} and {id_2} is {distance}")
+        cos = cosine_dist(embed_1, embed_2).real
+        has_related = check_related_study_exists(edges_df=edges_df, id_1=id_1, id_2=id_2)
+        # print(f"distance between projects {id_1} and {id_2} is {distance}")
+        res.append({
+            'study_1' : id_1,
+            'study_2' : id_2, 
+            'l_2': distance, 
+            'cosine':cos, 
+            'has_related' : has_related,
+            'x' : x
+        })
+        x = x + 1
+    ## exploration stuff 
+    ## plotting
+    distance_df = pd.DataFrame(res)
+    # import matplotlib.pyplot as plt
+    # color = ['red' if x else 'blue' for x in distance_df['has_related']]
+    # plt.scatter(x = distance_df['x'], y = distance_df['distance'], c=color)
+    # plt.xlabel('index')
+    # plt.ylabel('distance')
+    # plt.show()
+    # ## TSNE
+    # from sklearn.manifold import TSNE
+    # entity_ids = [entity_to_id[x] for x in all_project_ids]
+    # embeddings = model.entity_representations[0](indices=torch.as_tensor(entity_ids)).detach().cpu().numpy()
+    # tsne_embeddings = TSNE(n_components=2, learning_rate='auto',
+    #               init='random', perplexity=3).fit_transform(embeddings)
+    # plt.scatter(x = tsne_embeddings[:, 0], y = tsne_embeddings[:, 1],)
+    # plt.show()
+
+    # fig, ax = plt.subplots()
+    # ax.scatter(x = tsne_embeddings[:, 0], y = tsne_embeddings[:, 1],)
+    # for i, label in enumerate(all_project_ids):
+    #     ax.text(tsne_embeddings[i, 0] + 0.05, tsne_embeddings[i, 1] + 0.05, label, fontsize=9)
+    # plt.show()
+
+    # ## point based corelation 
+    from scipy.stats import pointbiserialr
+
+    r_l2, pval = pointbiserialr(distance_df['has_related'], distance_df['l_2'])
+    r_cos, pval = pointbiserialr(distance_df['has_related'], distance_df['cosine'])
+    print(r_l2)
+    print(r_cos)
